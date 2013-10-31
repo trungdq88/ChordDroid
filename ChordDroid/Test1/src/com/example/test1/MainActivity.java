8d0392557e175be2bfec8281b3297ae488cdbfe1
Đinh Quang Trung <trungdq88@gmail.com>
2013-10-31 21:51:33 +0700

    Open with eclipse

--- ChordDroid/Test1/src/com/example/test1/MainActivity.java
+++ ChordDroid/Test1/src/com/example/test1/MainActivity.java
@@ -81,6 +81,7 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
+		
 		return true;
 	}
 
